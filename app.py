c083009de487c4ce685928a8726deae474835478
niels-oz <enbeoh@gmail.com>
2023-06-09 23:23:42 +0200

    added some defaults

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import openai
 
 
-example_content = \
+EXAMPLE_CONTENT = \
 '''Himpelchen und Pimpelchen stiegen auf einen Berg.
 Himpelchen war ein Heinzelmann und
 Pimpelchen ein Zwerg.
@@ -15,7 +15,7 @@ sind sie in den Berg gekrochen.
 Da schlafen sie in guter Ruh.
 Seid mal still und hör gut zu!'''
 
-example_task = \
+EXAMPLE_TASK = \
 '''Your task is to generate a short summary of the content in 1 sentence. Then come up with a descriptive headline. \
 The headline has to be less than 30 characters in length. 
 
@@ -25,8 +25,13 @@ Summary: <your summary goes here>
 
 Headline: <your headline goes here>'''
 
+SYSTEM_ROLE = '''Use the provided content delimited by triple quotes to answer questions. If the answer cannot be \
+found in the content, write "I could not find an answer."'''
 
-def get_completion(prompt, role='', model='gpt-3.5-turbo', temperature=0, max_tokens=1600, top_p=.95,
+MODEL = 'gpt-3.5-turbo'
+
+
+def get_completion(prompt, role=SYSTEM_ROLE, model=MODEL, temperature=0, max_tokens=1600, top_p=.95,
                    frequency_penalty=0, presence_penalty=0, stop=None):
     messages = [{'role': 'system', 'content': role},{'role': 'user', 'content': prompt}]
     response = openai.ChatCompletion.create(
@@ -50,14 +55,15 @@ def main():
     st.title('Prompt Guru')
     st.header('A tool to improve your Chat GPT prompt 💬')
 
-    content = st.text_area('Content:', value=example_content, height=200)
-    prompt = st.text_area('Chat GPT Task:', value=example_task, height=200)
-
-    # (+) the content is delimited by triple backticks.  ``` ```
+    content = st.text_area('Content:', value=EXAMPLE_CONTENT, height=200)
+    prompt = st.text_area('Chat GPT Task:', value=EXAMPLE_TASK, height=200)
 
     if st.button('Process'):
-        response = get_completion(f'{prompt} \n \n Content: {content}')
+        response = get_completion(f'{prompt} \n \n Content: """{content}"""')
         st.write(response)
+
+    settings = st.json({'role': SYSTEM_ROLE, 'model': MODEL, 'temperature': 0.0, 'max_tokens': 1600, 'top_p': 0.95,
+                        'frequency_penalty': 0, 'presence_penalty': 0, 'stop': None})
 
 
 if __name__ == '__main__':
