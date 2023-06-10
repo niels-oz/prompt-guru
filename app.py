ace0095f248d3919a6ed0baca34ef73d318feb40
niels-oz <enbeoh@gmail.com>
2023-06-10 10:36:06 +0200

    example task + advanced settings

--- app.py
+++ app.py
@@ -3,36 +3,11 @@ import streamlit as st
 import os
 import openai
 
-
-EXAMPLE_CONTENT = \
-'''Himpelchen und Pimpelchen stiegen auf einen Berg.
-Himpelchen war ein Heinzelmann und
-Pimpelchen ein Zwerg.
-Sie blieben lange dort oben sitzen
-und wackelten mit ihren Zipfelmützen.
-Doch nach dreiunddreißig Wochen
-sind sie in den Berg gekrochen.
-Da schlafen sie in guter Ruh.
-Seid mal still und hör gut zu!'''
-
-EXAMPLE_TASK = \
-'''Your task is to generate a short summary of the content in 1 sentence. Then come up with a descriptive headline. \
-The headline has to be less than 30 characters in length. 
-
-Structure your output like the following example.
-
-Summary: <your summary goes here>
-
-Headline: <your headline goes here>'''
-
-SYSTEM_ROLE = '''Use the provided content delimited by triple quotes to answer questions. If the answer cannot be \
-found in the content, write "I could not find an answer."'''
-
-MODEL = 'gpt-3.5-turbo'
+from defaults import MODEL, SYSTEM_ROLE, EXAMPLE_CONTENT, EXAMPLE_TASK
 
 
-def get_completion(prompt, role=SYSTEM_ROLE, model=MODEL, temperature=0, max_tokens=1600, top_p=.95,
-                   frequency_penalty=0, presence_penalty=0, stop=None):
+def get_completion(prompt, role=SYSTEM_ROLE, model=MODEL, temperature=0, max_tokens=1600,  # TODO: calc max. tokens
+                   top_p=1.0, frequency_penalty=0, presence_penalty=0, stop=None):
     messages = [{'role': 'system', 'content': role},{'role': 'user', 'content': prompt}]
     response = openai.ChatCompletion.create(
         model=model,
@@ -52,18 +27,26 @@ def main():
     _ = load_dotenv()  # sets env vars as defined in .env
     openai.api_key = os.environ['OPENAI_API_KEY']
 
+    st.set_page_config(layout='wide')
+
     st.title('Prompt Guru')
-    st.header('A tool to improve your Chat GPT prompt 💬')
+    st.subheader('A tool to iteratively improve your Chat GPT prompt 💬')
 
-    content = st.text_area('Content:', value=EXAMPLE_CONTENT, height=200)
-    prompt = st.text_area('Chat GPT Task:', value=EXAMPLE_TASK, height=200)
+    col1, col2 = st.columns([2,1])
 
-    if st.button('Process'):
-        response = get_completion(f'{prompt} \n \n Content: """{content}"""')
-        st.write(response)
+    content = col1.text_area('Content:', value=EXAMPLE_CONTENT, height=100)
+    prompt = col1.text_area('Chat GPT Task:', value=EXAMPLE_TASK, height=200)
 
-    settings = st.json({'role': SYSTEM_ROLE, 'model': MODEL, 'temperature': 0.0, 'max_tokens': 1600, 'top_p': 0.95,
-                        'frequency_penalty': 0, 'presence_penalty': 0, 'stop': None})
+    with col2.expander('Advanced settings'):
+        st.write('If your task falls in the category of creative writing you can consider adjusting these values.')
+        temperature = st.slider('temperature (0 = kinda deterministic, 2 = as creative as it gets)', 0.0, 2.0, 0.0)
+        frequency_penalty = st.slider('frequency_penalty (↑ = don\'t repeat yourself)', 0.0, 2.0, 0.0)
+        presence_penalty = st.slider('presence_penalty (↑ = use less common phrases)', 0.0, 2.0, 0.0)
+
+    if col1.button('Process'):
+        response = get_completion(f'{prompt} \n \n Content: """{content}"""', temperature=temperature,
+                                  frequency_penalty=frequency_penalty, presence_penalty=presence_penalty)
+        col1.write(response)
 
 
 if __name__ == '__main__':
---
+++ defaults.py
@@ -0,0 +1,27 @@
+MODEL = 'gpt-3.5-turbo'
+SYSTEM_ROLE = '''Use the provided content delimited by triple quotes to answer questions. If the answer cannot be \
+found in the content, write "I could not find an answer."'''
+
+EXAMPLE_CONTENT = \
+'''Himpelchen und Pimpelchen stiegen auf einen Berg.
+Himpelchen war ein Heinzelmann und
+Pimpelchen ein Zwerg.
+Sie blieben lange dort oben sitzen
+und wackelten mit ihren Zipfelmützen.
+Doch nach dreiunddreißig Wochen
+sind sie in den Berg gekrochen.
+Da schlafen sie in guter Ruh.
+Seid mal still und hör gut zu!'''
+
+EXAMPLE_TASK = \
+'''Your task is to generate a short summary of the content in 1 sentence. Then come up with a descriptive headline. \
+The headline has to be less than 30 characters in length. 
+
+Structure your output like the following example.
+
+Summary: <your summary goes here>
+
+Headline: <your headline goes here>'''
+
+
+
