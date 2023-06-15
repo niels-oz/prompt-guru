3818b0058facdb5f4c5e5f54edb9cbb0a5b76c4a
niels-oz <enbeoh@gmail.com>
2023-06-15 23:15:17 +0200

    mock version

--- app.py
+++ app.py
@@ -30,75 +30,69 @@ def load_session(session):
     pass
 
 
-def main():
-    _ = load_dotenv()  # sets env vars as defined in .env
-    openai.api_key = os.environ['OPENAI_API_KEY']
+_ = load_dotenv()  # sets env vars as defined in .env
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-    if 'session' not in locals():
-        session = []
+if 'session' not in globals():
+    session = []
 
-    if 'cur_position' not in locals():
-        cur_position = -1
+if 'cur_position' not in globals():
+    cur_position = -1
 
-    st.set_page_config(layout='wide')
-    # st.markdown('<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;}</style>', unsafe_allow_html=True)
-    st.markdown('<style>button {height: auto;padding-top: 10px !important;padding-bottom: 10px !important;}</style>',
-                unsafe_allow_html=True,)
+st.set_page_config(layout='wide')
+# st.markdown('<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;}</style>', unsafe_allow_html=True)
+st.markdown('<style>button {height: auto;padding-top: 10px !important;padding-bottom: 10px !important;}</style>',
+            unsafe_allow_html=True,)
 
-    st.title('Prompt Guru')
-    st.subheader('A tool to iteratively improve your ChatGPT prompt 💬')
+st.title('Prompt Guru')
+st.subheader('A tool to iteratively improve your ChatGPT prompt 💬')
 
-    col1, col2 = st.columns([2, 1])
+col1, col2 = st.columns([2, 1])
+col21, col22 = col2.columns([1, 8])
 
-    content = col1.text_area('Content:', value=EXAMPLE_CONTENT, height=100)
-    prompt = col1.text_area('ChatGPT Task:', value=EXAMPLE_TASK, height=200)
+with col22.expander('Prompt upload'):
+    pg_prompt = st.file_uploader(label='', type="application/json")
+    # parse the file and load session
 
-    col21, col22, col23, col24 = col2.columns([1, 1, 1, 6])
+with col2.expander('Advanced settings'):
+    st.write('If your task falls in the category of creative writing you can consider adjusting these values.')
+    temperature = st.slider('temperature (0 = kinda deterministic, 2 = as creative as it gets)', 0.0, 2.0, 0.0)
+    frequency_penalty = st.slider('frequency_penalty (↑ = don\'t repeat yourself)', 0.0, 2.0, 0.0)
+    presence_penalty = st.slider('presence_penalty (↑ = use less common phrases)', 0.0, 2.0, 0.0)
 
-    if col21.button('◀', disabled=True if cur_position < 1 else False, help='previous prompt'):
-        cur_position -= 1
-        load_session(session[cur_position])
+content = col1.text_area('Content:', value=EXAMPLE_CONTENT, height=100)
+prompt = col1.text_area('ChatGPT Task:', value=EXAMPLE_TASK, height=200)
 
-    if cur_position >= 0:
-        json_string = json.dumps(session[cur_position])
-        timestamp = datetime.fromtimestamp(session[cur_position]['response']['created']).strftime('%Y%m%dT%H%M%S')
-    else:
-        json_string = '{}'
-        timestamp = ''
+if col1.button('Process'):
+    cur_request, response = get_completion(f'{prompt} \n\n Content: """{content}"""', role=SYSTEM_ROLE, model=MODEL,
+                                           max_tokens=1600, top_p=1.0, stop=None, temperature=temperature,
+                                           frequency_penalty=frequency_penalty, presence_penalty=presence_penalty)
 
-    button_download = col22.download_button(
-        label='💾',
-        disabled=True if cur_position < 0 else False,
-        file_name=f'prompt_guru_{timestamp}.json',
-        mime="application/json",
-        data=json_string,
-        help='download prompt'
-    )
+    session.append(cur_request)
+    cur_position = len(session) - 1  # mockup implementation turn into class eventually
+    col1.write(response)
 
-    if col23.button('▶', disabled=True if cur_position == len(session) - 1 else False, help='next prompt'):
-        cur_position += 1
-        load_session(session[cur_position])
+# if col21.button('◀', disabled=True if cur_position < 1 else False, help='previous prompt'):
+#     cur_position -= 1
+#     load_session(session[cur_position])
 
-    with col24.expander('Prompt upload'):
-        pg_prompt = st.file_uploader(label='', type="application/json")
+if cur_position >= 0:
+    json_string = json.dumps(session[cur_position])
+    timestamp = datetime.fromtimestamp(session[cur_position]['response']['created']).strftime('%Y%m%dT%H%M%S')
+else:
+    json_string = '{}'
+    timestamp = ''
 
-    with col2.expander('Advanced settings'):
-        st.write('If your task falls in the category of creative writing you can consider adjusting these values.')
-        temperature = st.slider('temperature (0 = kinda deterministic, 2 = as creative as it gets)', 0.0, 2.0, 0.0)
-        frequency_penalty = st.slider('frequency_penalty (↑ = don\'t repeat yourself)', 0.0, 2.0, 0.0)
-        presence_penalty = st.slider('presence_penalty (↑ = use less common phrases)', 0.0, 2.0, 0.0)
+button_download = col21.download_button(
+    label='💾',
+    disabled=True if cur_position < 0 else False,
+    file_name=f'prompt_guru_{timestamp}.json',
+    mime="application/json",
+    data=json_string,
+    help='download prompt'
+)
 
-    if col1.button('Process'):
-        cur_request, response = get_completion(f'{prompt} \n\n Content: """{content}"""', role=SYSTEM_ROLE, model=MODEL,
-                                               max_tokens=1600, top_p=1.0, stop=None, temperature=temperature,
-                                               frequency_penalty=frequency_penalty, presence_penalty=presence_penalty)
+# if col23.button('▶', disabled=True if cur_position == len(session) - 1 else False, help='next prompt'):
+#     cur_position += 1
+#     load_session(session[cur_position])
 
-        session.append(cur_request)
-        cur_position = len(session) - 1  # mockup implementation turn into class eventually
-        col1.write(response)
-
-    # stars = st_star_rating('Your rating', maxValue=5, defaultValue=0, key="rating")
-
-
-if __name__ == '__main__':
-    main()
